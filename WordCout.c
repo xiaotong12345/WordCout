961af78bb3f20c90eb87ed722a11bf6bcc6a22ec
xiaotong12345 <tongmingyang666@163.com>
2024-05-18 16:11:36 +0800

    second

--- WordCout.c
+++ WordCout.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+
+//Second submission
 int main() {
     char s[1000];
     int wordCount = 0;
     int inWord = 0;
     int i;
 
-    printf("输入: ");
+    printf("Please enter your statement: ");
     scanf("%[^\n]s", s);
 
     for (i = 0; s[i] != '\0'; i++) {
@@ -22,7 +24,7 @@ int main() {
         }
     }
 
-    printf("一共有 %d 个单词\n", wordCount);
+    printf("There are a total of %d words in your sentence\n", wordCount);
 
     return 0;
 }
